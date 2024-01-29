5b8fdf2f5b9b22d25a897c93a1dcd44348f15864
LiuYang-maker <1330163734@qq.com>
2024-01-29 22:07:39 +0800

    加了部分注释

--- models/pointnet2_sem_seg.py
+++ models/pointnet2_sem_seg.py
@@ -4,22 +4,35 @@ from models.pointnet2_utils import PointNetSetAbstraction,PointNetFeaturePropaga
 
 
 class get_model(nn.Module):
+    # get_model 类继承自 nn.Module，是一个PyTorch模型类。
     def __init__(self, num_classes):
         super(get_model, self).__init__()
+        # 在构造函数 __init__ 中，
+        # 定义了PointNet++中的SetAbstraction层和Feature Propagation层，
+        # 以及最终的全连接层。
+
+        # Set Abstraction layers
         self.sa1 = PointNetSetAbstraction(1024, 0.1, 32, 9 + 3, [32, 32, 64], False)
         self.sa2 = PointNetSetAbstraction(256, 0.2, 32, 64 + 3, [64, 64, 128], False)
         self.sa3 = PointNetSetAbstraction(64, 0.4, 32, 128 + 3, [128, 128, 256], False)
         self.sa4 = PointNetSetAbstraction(16, 0.8, 32, 256 + 3, [256, 256, 512], False)
+        # Feature Propogation layers
         self.fp4 = PointNetFeaturePropagation(768, [256, 256])
         self.fp3 = PointNetFeaturePropagation(384, [256, 256])
         self.fp2 = PointNetFeaturePropagation(320, [256, 128])
         self.fp1 = PointNetFeaturePropagation(128, [128, 128, 128])
+        # 全连接层和输出层定义
         self.conv1 = nn.Conv1d(128, 128, 1)
         self.bn1 = nn.BatchNorm1d(128)
         self.drop1 = nn.Dropout(0.5)
         self.conv2 = nn.Conv1d(128, num_classes, 1)
 
     def forward(self, xyz):
+        # forward 方法实现了前向传播，
+        # 接受点云数据 xyz 作为输入，
+        # 经过多个Set Abstraction和Feature Propagation层后，
+        # 最终输出预测结果 x 和中间特征 l4_points。
+        
         l0_points = xyz
         l0_xyz = xyz[:,:3,:]
 
@@ -41,6 +54,11 @@ class get_model(nn.Module):
 
 
 class get_loss(nn.Module):
+    '''
+    get_loss 类继承自 nn.Module，是一个用于计算损失的PyTorch模型类。
+    forward 方法接受预测结果 pred、目标标签 target、转置特征 trans_feat 和权重 weight，
+    使用负对数似然损失 F.nll_loss 计算总体损失。
+    '''
     def __init__(self):
         super(get_loss, self).__init__()
     def forward(self, pred, target, trans_feat, weight):
@@ -49,6 +67,11 @@ class get_loss(nn.Module):
         return total_loss
 
 if __name__ == '__main__':
+    '''
+    在脚本的末尾，通过创建一个 get_model 实例，
+    随机生成一个形状为 (6, 9, 2048) 的点云数据 xyz，
+    并通过模型进行前向传播得到输出。
+    '''
     import  torch
     model = get_model(13)
     xyz = torch.rand(6, 9, 2048)
